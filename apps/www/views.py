c5ab90d4205535d80f9920b4f89dcca9414aba1c
admin <tjnu1830080255@163.com>
2023-07-14 11:37:34 +0800

    re_path

--- apps/www/views.py
+++ apps/www/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse
 
 
 # Create your views here.
 
 def login(request):
+    result = reverse("n1")
+    print(result)
     return HttpResponse("欢迎登录！！！")
+
+
+def info(request, v1):
+    print(v1)
+    return HttpResponse(f"欢迎{v1}登录！！")
+
+
+def yy(request, a1, a2, a3):
+    print(a1, a2, a3)
+    return HttpResponse(f"{a1}-{a2}-{a3}")
+
+
--- djangoProject_V1/urls.py
+++ djangoProject_V1/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from apps.www import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', views.login),
+    path('login/', views.login, name='n1'),
+    path('info/<int:v1>/', views.info),
+    re_path(r'yy/(\d{4})-(\d{2})-(\d{2})/', views.yy),
 ]
